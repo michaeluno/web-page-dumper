7d8646a32ade580b40d217d24dbc71fba3986056
Michael Uno <michael2@michaeluno.jp>
2020-12-03 05:53:39 +0900

    [UI] Show more details about the error.

--- source/app.js
+++ source/app.js
@@ -90,9 +90,12 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
   // set locals, only providing error in development
   res.locals.status  = err.status || 500;
-  res.locals.message = err.message;
+  res.locals.message = 500 === res.locals.status
+    ? 'Internal Server Error'
+    : err.message;
   res.locals.error   = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
