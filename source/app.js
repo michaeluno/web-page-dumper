821398e3f99c6f489ae60d8978e0bc51680e6f4f
Michael Uno <michael2@michaeluno.jp>
2020-12-03 05:09:06 +0900

    [UI] Add the stack trace when the `dev` query argument is passed.

--- source/app.js
+++ source/app.js
@@ -93,6 +93,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.dev = parseInt( req.query.dev );
 
   // render the error page
   res.status( err.status || 500 );
