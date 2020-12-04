154883cac98529e794419820a5f3a3da34b383c3
Michael Uno <michael2@michaeluno.jp>
2020-12-05 01:10:26 +0900

    Compress responses.
    This is done by introducing the compression module.

--- source/app.js
+++ source/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require( 'compression' );
 
 // Additional dependencies
 
@@ -16,9 +17,10 @@ var multer = require('multer');
 var upload = multer();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users'); // @deprecated
 
 var app = express();
+app.use(compression({}));
 
 // Custom Data
 var projectData = {
