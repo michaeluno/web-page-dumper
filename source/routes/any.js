b9ac5ee91814412ea4f56deee7f1be805368b3cd
Michael Uno <michael2@michaeluno.jp>
2020-12-06 08:47:18 +0900

    [UI] Add the navigation menu and icons.

--- source/routes/any.js
+++ source/routes/any.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.all('*', function(req, res, next ) {
   res.locals.title = req.app.get( 'config' ).project.name;
-  res.locals.homeURL = req.protocol+"://"+req.headers.host;
+  res.locals.urlHome  = req.protocol + "://" + req.headers.host;
+  res.locals.urlUsage = req.protocol + "://" + req.headers.host + '/usage';
   next();
 });
 module.exports = router;
